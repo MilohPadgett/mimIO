96772115ab783f5cb74a5ab9f9b5839585a35694
MilohPadgett <padgettmiloh@gmail.com>
2023-04-17 18:54:18 -0700

    updated

--- head_track.py
+++ head_track.py
@@ -2,13 +2,15 @@ import cv2
 import numpy as np
 import mediapipe as mp
 import RPi.GPIO as GPIO
+import pigpio
 from time import sleep
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(32, GPIO.OUT)
+yaw_servo = 26
+pwm = pigpio.pi()
+pwm.set_mode(yaw_servo, pigpio.OUTPUT)
+pwm.set_PWM_frequency(yaw_servo, 50)
 
-pwm=GPIO.PWM(32, 50)
-pwm.start(0)
+lock = 0
 
 #mediapipe inits
 mp_face_mesh = mp.solutions.face_mesh
@@ -16,9 +18,9 @@ face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_con
 mp_drawing = mp.solutions.drawing_utils
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
-curr=0
-ma = [0]*5
-i = 0
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
 
 cap = cv2.VideoCapture(0)
 while cap.isOpened():
@@ -27,6 +29,7 @@ while cap.isOpened():
 
     image.flags.writeable = False
     results = face_mesh.process(image)
+    pose_res = pose.process(image)
     image.flags.writeable = True
     
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -35,7 +38,11 @@ while cap.isOpened():
     img_h, img_w, img_c = image.shape
     face_3d = []
     face_2d = []
-
+    
+    if pose_res.pose_landmarks:
+        print(pose_res.pose_landmarks.landmark[14].x*img_w)
+        print(pose_res.pose_landmarks.landmark[14].y*img_h)
+    
     if results.multi_face_landmarks:
         for face_landmarks in results.multi_face_landmarks:
             for idx, lm in enumerate(face_landmarks.landmark):
@@ -81,26 +88,25 @@ while cap.isOpened():
             #keep array and shift right 
             #also maybe step instead of jumping
             #translate angles to degrees for servo motor
-            """!
+    
+            """
             if x:
                 pitch = ((x+4.5)/14)*120
                 if pitch>0 and pitch<120:
                     pin11.write(pitch)
-            """
             
-            if y:
+            
+            if y and abs(y-lock)>1:
+                lock=y
                 yaw = ((y+10)/17)*180
                 if yaw>0 and yaw<180:
-                    dc = (9.5*yaw)/180 + 1.5
 
-                    pwm.ChangeDutyCycle(dc)
-
-                    
-                    
-                    
-
-            
+                    pw = 500+yaw*8.33
+                    #print(pw)
+                    pwm.set_servo_pulsewidth(yaw_servo,pw)
+            """
             # Display the nose direction
+            
             nose_3d_projection, jacobian = cv2.projectPoints(nose_3d, rot_vec, trans_vec, cam_matrix, dist_matrix)
 
             p1 = (int(nose_2d[0]), int(nose_2d[1]))
@@ -111,15 +117,21 @@ while cap.isOpened():
             # Add the text on the image
             cv2.putText(image, "x: " + str(np.round(x,2)), (500, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             cv2.putText(image, "y: " + str(np.round(y,2)), (500, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    
-        """!
+            
+
         mp_drawing.draw_landmarks(
                     image=image,
                     landmark_list=face_landmarks,
                     connections=mp_face_mesh.FACEMESH_TESSELATION,
                     landmark_drawing_spec=drawing_spec,
                     connection_drawing_spec=drawing_spec)
-        """
+            
+    mp_drawing.draw_landmarks(image=image,
+                landmark_list=pose_res.pose_landmarks,
+                connections=mp_pose.POSE_CONNECTIONS,
+                landmark_drawing_spec=drawing_spec,
+                connection_drawing_spec=drawing_spec)
+        
 
     cv2.imshow("Head Pose", image)
     if cv2.waitKey(5) & 0xFF == ord('q'):
@@ -127,4 +139,3 @@ while cap.isOpened():
 cap.release()
 cv2.destroyAllWindows()
 pwm.stop()
-GPIO.cleanup()
